ca446133f7a332e4707d0e811775e17407ea951d
Guillaume Lebigot <axel@teri-chan.net>
2019-06-06 16:48:39 +0200

    Added timer

--- utils/date.ts
+++ utils/date.ts
@@ -73,3 +73,33 @@ export function duration(duration: number): string {
 	if (seconds !== 0) returnString = returnString + `${seconds} ${i18n.__('SECOND')} `;
 	return returnString;
 }
+
+export function timer(callback: any, delay: number) {
+	let id: any, started: Date, remaining: number = delay, running : boolean;
+
+	this.start = () => {
+		running = true;
+		started = new Date();
+		id = setTimeout(callback, remaining);
+	};
+
+	this.pause = () => {
+		running = false;
+		clearTimeout(id);
+		remaining -= new Date().getTime() - started.getTime();
+	};
+
+	this.getTimeLeft = () => {
+		if (running) {
+			this.pause();
+			this.start();
+		}
+		return remaining;
+	};
+
+	this.getStateRunning = () => {
+		return running;
+	};
+
+	this.start();
+}
