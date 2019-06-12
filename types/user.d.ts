add35428b15cad4416f6a80590ab1bd955d39e75
Guillaume Lebigot <axel@teri-chan.net>
2019-06-12 11:33:23 +0200

    Added user types

---
+++ types/user.d.ts
@@ -0,0 +1,26 @@
+export type Role = 'user' | 'guest' | 'admin';
+
+export interface Token {
+	username: string,
+	role: Role,
+	token?: string,
+	onlineToken?: string
+}
+
+export interface User {
+	login?: string,
+	old_login?: string,
+	type?: number,
+	avatar_file?: string,
+	bio?: string,
+	url?: string,
+	email?: string,
+	nickname?: string,
+	password?: string,
+	last_login_at?: Date,
+	flag_online?: boolean,
+	onlineToken?: string,
+	series_lang_mode?: number,
+	main_series_lang?: string,
+	fallback_series_lang?: string
+}
