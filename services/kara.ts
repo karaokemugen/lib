cf24461fe5fad12c257605748bbb0a5d87bf1ef2
Guillaume Lebigot <axel@teri-chan.net>
2019-07-15 13:48:39 +0200

    Save space by not sending i18n if tag only contains one trnaslation === its name

--- services/kara.ts
+++ services/kara.ts
@@ -6,8 +6,14 @@ export function consolidatei18n(data: any): i18nData {
 	for (const i in data) {
 		for (const type of Object.keys(tagTypes)) {
 			if (data[i][type]) for (const y in data[i][type]) {
-				if (!i18n[data[i][type][y].tid]) {
-					i18n[data[i][type][y].tid] = {...data[i][type][y].i18n}
+				const tag = data[i][type][y];
+				if (!i18n[tag.tid]) {
+					const translations = Object.keys(tag.i18n);
+					if (translations.length > 1) {
+						i18n[tag.tid] = {...tag.i18n}
+					} else if (translations[translations[0]] === tag.name) {
+						// Do not create a i18n item if there is only one translation and it's the exact same thing as the tag name
+					}
 					delete data[i][type][y].i18n;
 				}
 			}
