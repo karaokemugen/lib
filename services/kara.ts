71e485d191845f2814f265ec1094a06bee9d344d
Guillaume Lebigot <axel@teri-chan.net>
2019-08-21 14:58:06 +0200

    Remove unused i18n data from tags (requires call to formatKaraList to be modified)

--- services/kara.ts
+++ services/kara.ts
@@ -1,8 +1,16 @@
 import { tagTypes } from "../utils/constants";
 import { i18nData } from "../types/database/kara";
 
+/** Remove unused languages from a i18nData object */
+export function removeUnusedLangs(i18n: i18nData, langs: string[]): i18nData {
+	for (const lang of Object.keys(i18n)) {
+		if (!langs.includes(lang)) delete i18n[lang];
+	}
+	return i18n;
+}
+
 /** Pick all i18n items from tags in karalist, consolidate them and remove duplicates */
-export function consolidatei18n(data: any): i18nData {
+export function consolidatei18n(data: any, langs: string[]): i18nData {
 	const i18n = {};
 	for (const i in data) {
 		for (const type of Object.keys(tagTypes)) {
@@ -11,9 +19,11 @@ export function consolidatei18n(data: any): i18nData {
 				if (!i18n[tag.tid]) {
 					const translations = Object.keys(tag.i18n);
 					if (translations.length > 1) {
-						i18n[tag.tid] = {...tag.i18n}
+						//i18n[tag.tid] = {...tag.i18n};
+						i18n[tag.tid] = removeUnusedLangs({...tag.i18n}, langs);
 					} else if (tag.i18n[translations[0]] !== tag.name) {
-						i18n[tag.tid] = {...tag.i18n}
+						//i18n[tag.tid] = {...tag.i18n};
+						i18n[tag.tid] = removeUnusedLangs({...tag.i18n}, langs);
 					}
 					delete data[i][type][y].i18n;
 				}
