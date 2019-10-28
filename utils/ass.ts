9e039e7dc23965af258dc9da7a94f18286695709
Guillaume Lebigot <axel@teri-chan.net>
2019-10-28 10:10:47 +0100

    Added sort to lyrics output by Start property

--- utils/ass.ts
+++ utils/ass.ts
@@ -3,6 +3,7 @@ import {parse as assParser} from 'ass-compiler';
 /** Parse ASS data and return lyrics */
 export function ASSToLyrics(ass: string): string[] {
 	const script = assParser(ass);
+	script.events.dialogue.sort((a, b) => (a.Start > b.Start) ? 1 : -1 );
 	return script.events.dialogue.map(dialogue => dialogue.Text.combined);
 }
 
