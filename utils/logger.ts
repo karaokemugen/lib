020435f1421f1cfb35ec1af8b703df05e3a18b3a
Guillaume Lebigot <axel@teri-chan.net>
2020-01-07 17:19:56 +0100

    Revert "Use chalk in logger to display beautifully colored messages"
    This reverts commit 776354ea385b1fa4bdf114052940943395125420.
    
    That was a bad idea, it truncates pasted objects for some reason.

--- utils/logger.ts
+++ utils/logger.ts
@@ -4,34 +4,8 @@ import {resolve} from 'path';
 import {date, time} from './date';
 import dailyRotateFile from  'winston-daily-rotate-file';
 import { getState } from '../../utils/state';
-import chalk from 'chalk';
 
 export default logger;
-
-const moduleColors = {
-	BinCheck: chalk.white,
-	Blacklist: chalk.red,
-	Config: chalk.green,
-	DB: chalk.yellow,
-	Download: chalk.blue,
-	Downloader: chalk.magenta,
-	Engine: chalk.cyan,
-	Gen: chalk.grey,
-	KaraGen: chalk.rgb(10, 20, 100),
-	Launcher: chalk.rgb(100, 10, 20),
-	Player: chalk.rgb(100, 100, 200),
-	Playlist: chalk.rgb(100, 200, 100),
-	Poll: chalk.magentaBright,
-	Previews: chalk.cyanBright,
-	ShortURL: chalk.greenBright,
-	State: chalk.redBright,
-	Stats: chalk.blueBright,
-	Store: chalk.rgb(50, 100, 200),
-	Update: chalk.rgb(200, 200, 200),
-	User: chalk.whiteBright,
-	Webapp: chalk.rgb(50, 150, 150),
-	Whitelist: chalk.whiteBright
-}
 
 let profiling = false;
 
@@ -54,20 +28,12 @@ export async function configureLogger(dataPath: string, debug: boolean, rotate?:
 			format: logger.format.combine(
 				logger.format.colorize(),
 				logger.format.printf(info => {
-					//Finding out module. It's inbetween []
-					let module = info.message.split(']')[0].replace('[','');
-					let message = info.message.split(/\](.+)/)[1];
-					if (message) message = message.trim();
-					if (!module) module = 'KM';
-					let color = moduleColors[module];
-					if (!color) color = chalk.white;
-
 					let duration = '';
 					if (info.durationMs) duration = `duration: ${info.durationMs} ms`;
 					//Padding if info.level is 4 characters long only
 					let level = `${info.level}:`;
 					if (info.level === 'info' || info.level === 'warn') level = `${info.level}: `;
-					return `${time()} - ${level} ${color(module)}> ${message} ${duration}`;
+					return `${time()} - ${level} ${info.message} ${duration}`;
 				})
 			)
 		})
