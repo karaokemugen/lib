aa753ac993913d25968145aa035755c59b980e99
Aeden <florentius.b@gmail.com>
2020-02-07 22:18:56 +0100

    fix build

--- services/kara_creation.ts
+++ services/kara_creation.ts
@@ -43,7 +43,7 @@ export async function generateKara(kara: Kara, karaDestDir: string, mediasDestDi
 		genres: {tagValidator: true},
 		platforms: {tagValidator: true},
 		origins: {tagValidator: true},
-		seasons: {tagValidator: true],
+		seasons: {tagValidator: true},
 		title: {presence: true}
 	});
 	// Move files from temp directory to import, depending on the different cases.
