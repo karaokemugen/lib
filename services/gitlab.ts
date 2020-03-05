2b0ca002e4f1c563cf12bc454694848270adf826
Guillaume Lebigot <axel@teri-chan.net>
2020-03-05 17:26:58 +0100

    Revert "Made gitlabPostNewIssue more flexible"
    This reverts commit 534144adaed524dab0cff61480f229923af1ddca.

--- services/gitlab.ts
+++ services/gitlab.ts
@@ -4,16 +4,16 @@ import logger from '../utils/logger';
 import {findUserByName} from '../../services/user';
 
 /** Posts a new issue to gitlab and return its URL */
-export async function gitlabPostNewIssue(gitlabHost: string, projectID: number, title: string, desc: string, labels: string[]): Promise<string> {
+export async function gitlabPostNewIssue(title: string, desc: string, labels: string[]): Promise<string> {
 	const conf = getConfig();
 	if (!labels) labels = [];
 	const params = new URLSearchParams([
-		['id', `${projectID}`],
+		['id', `${conf.Gitlab.ProjectID}`],
 		['title', title],
 		['description', desc],
 		['labels', labels.join(',')]
 	]);
-	const res = await got.post(`${gitlabHost}/api/v4/projects/${projectID}/issues?${params.toString()}`, {
+	const res = await got.post(`${conf.Gitlab.Host}/api/v4/projects/${conf.Gitlab.ProjectID}/issues?${params.toString()}`, {
 		headers: {
 			'PRIVATE-TOKEN': conf.Gitlab.Token
 		}
@@ -22,15 +22,14 @@ export async function gitlabPostNewIssue(gitlabHost: string, projectID: number, 
 }
 
 export async function postSuggestionToKaraBase(title: string, serie:string, type:string, link:string, username: string): Promise<string> {
-	const conf = getConfig();
-	const confTemplate = conf.Gitlab.IssueTemplate;
-	let titleIssue = confTemplate && confTemplate.Suggestion && confTemplate.Suggestion.Title
-		? confTemplate.Suggestion.Title
+	const conf = getConfig().Gitlab.IssueTemplate;
+	let titleIssue = conf && conf.Suggestion && conf.Suggestion.Title
+		? conf.Suggestion.Title
 		: '[suggestion] $serie - $title';
 	titleIssue = titleIssue.replace('$title', title);
 	titleIssue = titleIssue.replace('$serie', serie);
-	let desc = confTemplate && confTemplate.Suggestion && confTemplate.Suggestion.Description
-		? confTemplate.Suggestion.Description
+	let desc = conf && conf.Suggestion && conf.Suggestion.Description
+		? conf.Suggestion.Description
 		: 'From $username : it would be nice if someone could time this!';
 	const user = await findUserByName(username);
 	desc = desc.replace('$username', user ? user.nickname : username);
@@ -39,7 +38,7 @@ export async function postSuggestionToKaraBase(title: string, serie:string, type
 	desc = desc.replace('$type', type);
 	desc = desc.replace('$link', link);
 	try {
-		return await gitlabPostNewIssue(conf.Gitlab.Host, conf.Gitlab.ProjectID || conf.Gitlab.BaseProjectID, titleIssue, desc, confTemplate.Suggestion.Labels);
+		return await gitlabPostNewIssue(titleIssue, desc, conf.Suggestion.Labels);
 	} catch(err) {
 		logger.error(`[KaraSuggestion] Call to Gitlab API failed : ${err}`);
 	}
