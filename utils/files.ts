b53caedb53bff3cffe2500d3b47501cc4632931a
Guillaume Lebigot <axel@teri-chan.net>
2020-05-24 16:55:17 +0200

    Try to fix asyncMove options

--- utils/files.ts
+++ utils/files.ts
@@ -215,6 +215,7 @@ export async function browseFs(dir: string, onlyMedias: boolean) {
 }
 
 export async function asyncMove(path1: string, path2: string, options?: any) {
+	if (!options) options = {};
 	if (path1 === path2) return;
 	return await asyncMoveFile(path1, path2, options);
 }
