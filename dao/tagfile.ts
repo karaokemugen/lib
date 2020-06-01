bc47e6d1e502e1c377bc8916ec3a3a147616944e
Guillaume Lebigot <axel@teri-chan.net>
2020-06-01 09:29:20 +0200

    Remove SID from kara when removing tags

--- dao/tagfile.ts
+++ dao/tagfile.ts
@@ -101,6 +101,7 @@ export async function removeTagInKaras(tid: string, karas: KaraList) {
 		logger.info(`[Kara] Removing in ${karaWithTag.karafile}...`);
 		const karaPath = await resolveFileInDirs(karaWithTag.karafile, resolvedPathRepos('Karas', karaWithTag.repository));
 		const kara = await parseKara(karaPath[0]);
+		if (kara.data.sids?.length > 0) kara.data.sids = kara.data.sids.filter(s => s !== tid);
 		for (const type of Object.keys(tagTypes)) {
 			if (kara.data.tags[type]) kara.data.tags[type] = kara.data.tags[type].filter((t: string) => t !== tid);
 			if (kara.data.tags[type]?.length === 0) delete kara.data.tags[type];
