70346d2492c14504663bdcb9179b4e7a5fec45a8
Guillaume Lebigot <axel@teri-chan.net>
2021-02-19 17:11:44 +0100

    DB: rework how karas/tags are added/edited/removed (FAST & FURIOUS EDITION)

--- dao/kara.ts
+++ dao/kara.ts
@@ -1,12 +1,44 @@
 import logger, { profile } from '../utils/logger';
 import { databaseReady, db, newDBTask } from './database';
-import { sqlUpdateKaraSearchVector } from './sql/kara';
+import { sqlCreateKaraIndexes, sqlRefreshKaraTable, sqlUpdateKaraSearchVector } from './sql/kara';
 
 export async function refreshKarasTask() {
 	profile('refreshKaras');
-	logger.debug('Refreshing karas view', {service: 'DB'});
-	await db().query('REFRESH MATERIALIZED VIEW CONCURRENTLY all_karas');
+	logger.debug('Refreshing karas table', {service: 'DB'});
+	await db().query(`DROP TABLE IF EXISTS all_karas_new;
+	CREATE TABLE all_karas_new AS ${sqlRefreshKaraTable([], [])};
+	DROP TABLE IF EXISTS all_karas;
+	ALTER TABLE all_karas_new RENAME TO all_karas;
+	`);
+	// Re-creating indexes is done asynchronously
+	db().query(sqlCreateKaraIndexes);
 	profile('refreshKaras');
+}
+
+export async function refreshKarasInsert(kid: string) {
+	await db().query(`INSERT INTO all_karas
+	${sqlRefreshKaraTable([` AND k.pk_kid = '${kid}'`], [])}
+	ON CONFLICT DO NOTHING`);
+}
+
+export async function refreshKarasDelete(kid: string) {
+	await db().query(`DELETE FROM all_karas WHERE pk_kid = '${kid}';`);
+}
+
+export async function refreshKarasUpdate(kid: string) {
+	await db().query(`DELETE FROM all_karas WHERE pk_kid = '${kid}';
+	INSERT INTO all_karas
+		${sqlRefreshKaraTable([` AND k.pk_kid = '${kid}'`], [])}
+		;
+	`);
+}
+
+export async function refreshKarasUpdateByTag(tid: string) {
+	await db().query(`DELETE FROM all_karas WHERE ARRAY_TO_STRING(tid,' ') LIKE '%${tid}%';
+	INSERT INTO all_karas
+		${sqlRefreshKaraTable([' AND ktall.fk_kid = k.pk_kid'], ['LEFT JOIN kara_tag ktall ON ktall.fk_kid = k.pk_kid'])}
+		;
+	`);
 }
 
 export async function refreshKaras() {
@@ -26,6 +58,6 @@ export async function refreshYears() {
 	await databaseReady();
 }
 
-export async function updateKaraSearchVector() {
-	return db().query(sqlUpdateKaraSearchVector);
+export async function updateKaraSearchVector(kid?: string) {
+	return db().query(sqlUpdateKaraSearchVector(kid));
 }
