fafae55a00e45c8d97b80f74c5cdc71e0633ce55
Guillaume Lebigot <axel@teri-chan.net>
2021-05-31 14:58:02 +0200

    Fix git diff

--- utils/git.ts
+++ utils/git.ts
@@ -3,7 +3,7 @@ import execa from 'execa';
 import {getState} from '../../utils/state';
 
 export async function gitDiff(commit1: string, commit2: string, gitDir: string): Promise<string> {
-	const res = await execa(getState().binPath.git, ['diff', `${commit1}^..${commit2}`], {
+	const res = await execa(getState().binPath.git, ['diff', `${commit1}..${commit2}`], {
 		encoding: 'utf8',
 		cwd: gitDir
 	});
