c4c4486b7f03be4271018f10a7ae9e1a8cf5f22a
Guillaume Lebigot <axel@teri-chan.net>
2021-05-31 14:57:25 +0200

    Revert "Modified git tools"
    This reverts commit 364bc9a2c7210217d3708cf6556dbb740d72806a.

--- utils/git.ts
+++ utils/git.ts
@@ -3,16 +3,6 @@ import execa from 'execa';
 import {getState} from '../../utils/state';
 
 export async function gitDiff(commit1: string, commit2: string, gitDir: string): Promise<string> {
-	// We need to get the commit next to the one we've been supplied, because the diff command also includes the commit we mention in its diff
-	// Which is pretty stupid, but git is often like that.
-	const nextCommitRes = await execa(getState().binPath.git, ['rev-list', '--reverse', '--ancestry-path', `${commit1}..HEAD`], {
-		encoding: 'utf8',
-		cwd: gitDir
-	});
-	const nextCommit = nextCommitRes.stdout.split('\n')[0];
-	// If nextCommit is empty, it means we're at HEAD, so no need to return anything else
-	if (nextCommit === '') return '';
-
 	const res = await execa(getState().binPath.git, ['diff', `${commit1}^..${commit2}`], {
 		encoding: 'utf8',
 		cwd: gitDir
