27625e81d206dec79d3ac7c57987ae1e453e6703
Leon <me@leonekmi.fr>
2022-02-06 23:20:46 +0100

    Add refreshKarasAfterDBChange in lib

--- services/karaManagement.ts
+++ services/karaManagement.ts
@@ -1,6 +1,51 @@
 import { Kara } from '../types/kara';
 import { tagTypes } from '../utils/constants';
-import { updateKaraTags } from '../dao/tag';
+import {refreshTags, updateKaraTags, updateTagSearchVector} from '../dao/tag';
+import logger, {profile} from '../utils/logger';
+import {
+	refreshKaras,
+	refreshKarasDelete,
+	refreshKarasInsert,
+	refreshKarasUpdate,
+	refreshParentSearchVectorTask,
+	refreshYears,
+	updateKaraSearchVector
+} from '../dao/kara';
+
+
+export async function refreshKarasAfterDBChange(action: 'ADD' | 'UPDATE' | 'DELETE' | 'ALL' = 'ALL', karas?: Kara[]) {
+	profile('RefreshAfterDBChange');
+	logger.debug('Refreshing DB after kara change', { service: 'DB' });
+	await updateKaraSearchVector();
+	if (action === 'ADD') {
+		await refreshKarasInsert(karas.map(k => k.kid));
+	} else if (action === 'UPDATE') {
+		await refreshKarasUpdate(karas.map(k => k.kid));
+	} else if (action === 'DELETE') {
+		await refreshKarasDelete(karas.map(k => k.kid));
+	} else if (action === 'ALL') {
+		await refreshKaras();
+	}
+	refreshYears();
+	const parentsToUpdate: Set<string> = new Set();
+	for (const kara of karas) {
+		if (kara.parents) {
+			for (const parent of kara.parents) {
+				parentsToUpdate.add(parent);
+			}
+		}
+	}
+	// If karas is not initialized then we're updating ALL search vectors
+	karas ? refreshParentSearchVectorTask([...parentsToUpdate]) : refreshParentSearchVectorTask();
+	refreshTagsAfterDBChange();
+	logger.debug('Done refreshing DB after kara change', { service: 'DB' });
+	profile('RefreshAfterDBChange');
+}
+
+async function refreshTagsAfterDBChange() {
+	await updateTagSearchVector();
+	refreshTags();
+}
 
 export async function updateTags(kara: Kara) {
 	const tagsAndTypes = [];
