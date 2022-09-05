eece1d02d6441a236bcb04110b81a0bdeefbca9b
Guillaume Lebigot <axel@teri-chan.net>
2022-09-05 15:50:33 +0200

    Removed download bundle type

--- types/downloads.d.ts
+++ types/downloads.d.ts
@@ -1,23 +1,6 @@
 import { KaraFileV4 } from './kara';
 import { TagFile } from './tag';
 
-// Old <5.1 download bundle. Remove when 5.0 and down gets out of the way
-export interface DownloadBundleServer extends DownloadBundle {
-	header: {
-		description: string;
-	};
-	kara: KaraMetaFile;
-	lyrics: MetaFile;
-	tags: TagMetaFile[];
-}
-
-export interface DownloadBundle {
-	name?: string;
-	uuid?: string;
-	size?: number;
-	mediaFile?: string;
-}
-
 interface MetaFile {
 	file: string;
 	data: any;
