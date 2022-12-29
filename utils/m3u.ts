44d23ae372558b4698235699b13400c609d69233
TheMio <themio@protonmail.ch>
2022-12-29 02:04:25 +0100

    🎨 use dayjs

--- utils/m3u.ts
+++ utils/m3u.ts
@@ -1,24 +1,26 @@
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
+import dayjs from 'dayjs';
 import { M3uMedia, M3uPlaylist } from 'm3u-parser-generator';
 
 import { DBPL, DBPLC } from '../types/database/playlist';
 
 export function M3uFromPlaylist(playlistInfo: DBPL, content: DBPLC[]) {
-    const m3uPlaylist = new M3uPlaylist();
-    m3uPlaylist.title = playlistInfo.name;
-    m3uPlaylist.medias = content.map(c => new M3uMedia(c.mediafile));
-    return m3uPlaylist.getM3uString();
+	const m3uPlaylist = new M3uPlaylist();
+	m3uPlaylist.title = playlistInfo.name;
+	m3uPlaylist.medias = content.map(c => new M3uMedia(c.mediafile));
+	return m3uPlaylist.getM3uString();
 }
-export async function generateM3uFileFromPlaylist(playlistInfo: DBPL, content: DBPLC[], directory: string) {
-    const m3uPlaylistContent = M3uFromPlaylist(playlistInfo, content);
-    const dateFormatted = new Date()
-        .toJSON() // Formatting date is a bit hacky when we have no library for it
-        .replace('T', '_')
-        .split(':')
-        .join('-')
-        .split('.')[0];
-    const m3uFileName = `${dateFormatted}_${playlistInfo?.name}.m3u`;
-    await writeFile(join(directory, m3uFileName), m3uPlaylistContent, { encoding: 'utf-8' });
+export async function generateM3uFileFromPlaylist(
+	playlistInfo: DBPL,
+	content: DBPLC[],
+	directory: string
+) {
+	const m3uPlaylistContent = M3uFromPlaylist(playlistInfo, content);
+	const dateFormatted = dayjs(new Date()).format('YYYY-MM-DD_HH-mm-ss');
+	const m3uFileName = `${dateFormatted}_${playlistInfo?.name}.m3u`;
+	await writeFile(join(directory, m3uFileName), m3uPlaylistContent, {
+		encoding: 'utf-8',
+	});
 }
