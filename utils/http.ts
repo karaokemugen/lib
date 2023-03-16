96c70265979b63526b62b5be1c99d635ebcff627
Guillaume Lebigot <axel@teri-chan.net>
2023-03-16 09:24:01 +0100

    Added fixedEncodeURIComponent function

--- utils/http.ts
+++ utils/http.ts
@@ -14,3 +14,9 @@ const HTTP = axios.create({
 });
 
 export default HTTP;
+
+export function fixedEncodeURIComponent(str: string): string {
+	return encodeURIComponent(str).replace(/[!'()*#]/g, c => {
+		return `%${c.charCodeAt(0).toString(16)}`;
+	});
+}
