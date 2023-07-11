e1d1630801dc448c850d78223be8699a0bd8dfa8
Guillaume Lebigot <axel@teri-chan.net>
2023-07-11 22:47:32 +0200

    Fix mes conneries

--- dao/tagfile.ts
+++ dao/tagfile.ts
@@ -103,7 +103,7 @@ export function tagDataValidationErrors(tagData: Tag) {
 
 export function defineTagFilename(tag: Tag, oldTag?: DBTag): string {
 	const newFile = `${sanitizeFile(tag.name)}.${tag.tid.substring(0, 8)}.tag.json`;
-	if (process.platform === 'win32' && newFile !== oldTag.tagfile && newFile.toLowerCase() === oldTag.tagfile.toLowerCase()) {
+	if (oldTag && process.platform === 'win32' && newFile !== oldTag.tagfile && newFile.toLowerCase() === oldTag.tagfile.toLowerCase()) {
 		return oldTag.tagfile;
 	}
 	return newFile;
