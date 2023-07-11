9f11cbd7299e5b281a792b81e4eae7e739943a5b
Guillaume Lebigot <axel@teri-chan.net>
2023-07-11 17:21:29 +0200

    Added tentative repomanifestV2 type and supported codecs

--- types/repo.d.ts
+++ types/repo.d.ts
@@ -1,3 +1,5 @@
+import { supportedAudioCodecs, supportedFiles, supportedVideoCodecs } from '../utils/constants.js';
+
 export interface RepositoryBasic {
 	Name: string;
 	Online: boolean;
@@ -62,4 +64,87 @@ export interface DiffChanges {
 	path: string;
 	uid?: string;
 	contents?: string;
+}
+
+export type VideoContainers = typeof supportedFiles.video[number];
+export type VideoCodecs = typeof supportedVideoCodecs[number];
+export type AudioContainers = typeof supportedFiles.audio[number];
+export type AudioCodecs = typeof supportedAudioCodecs[number];
+export type LyricsFormats = typeof supportedFiles.lyrics[number];
+
+interface RepositoryLyricsManifestASS {
+	name: 'ass',
+	removeGarbage?: boolean;
+	removeHeaderComments?: boolean;
+	removeUnusedStyles?: boolean;
+	removeUnusedFonts?: boolean;
+	setTitle?: boolean;
+	setOriginalTiming?: boolean;
+	set0x0Resolution?: boolean;
+}
+
+interface RepositoryLyricsManifest {
+	name: LyricsFormats
+}
+
+export interface RepositoryManifestV2 {
+	name: string,
+	description: string,
+	homeURL: string,
+	contentsURL: string,
+	gitURL: string,
+	rules: {
+		video?: {
+			containers?: {
+				mandatory?: boolean,
+				allowed: VideoContainers[]
+			},
+			resolution: {
+				min?: {
+					height: number,
+					width: number,
+					mandatory?: boolean
+				},
+				max?: {
+					height: number,
+					width: number,
+					mandatory?: boolean
+				}
+			},
+			codecs?: {
+				allowed: VideoCodecs[],
+			},
+			bitrate?: {
+				mandatory?: boolean,
+				min?: number,
+				max?: number,
+			},
+			colorSpace?: {
+				// FIXME: find out how to list ffmpeg colorspaces and put that in a const type
+				allowed: string[]
+				mandatory?: boolean
+			},
+		},
+		audio?: {
+			containers?: {
+				mandatory?: boolean;
+				allowed: AudioContainers[]
+			},
+			codecs?: {
+				allowed?: AudioCodecs[],
+				mandatory?: boolean,
+				bitrate?: {
+					mandatory?: boolean,
+					min?: number,
+					max?: number,
+				}
+			}
+			coverArt?: {
+				mandatory: boolean,
+			}
+		},
+		lyrics?: {
+			formats?: (RepositoryLyricsManifest | RepositoryLyricsManifestASS)[]
+		}
+	}
 }
