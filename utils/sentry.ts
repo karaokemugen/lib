24c7fc577eb4f5b206daf7c47ea8b766d509dc4a
Florent Berthelot <florentius.b@gmail.com>
2024-07-06 13:49:55 +0200

    🔥 Remove function override in app & server

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -15,34 +15,6 @@ export default class SentryLogger {
 		this.Sentry = sentry_sdk;
 	}
 
-	init() {
-		if (process.env.CI_SERVER || process.env.SENTRY_TEST) {
-			console.log('CI detected/SENTRY_TEST present - Sentry disabled');
-			console.log("Have a nice day, sentries won't fire at you~");
-			return;
-		}
-		if (!process.env.SENTRY_DSN) {
-			// No DSN provided, return.
-			return;
-		}
-		this.Sentry.init({
-			dsn: process.env.SENTRY_DSN,
-			environment: process.env.SENTRY_ENVIRONMENT || 'release',
-			release: getState().version.number,
-			dist: getState().version.sha,
-			beforeSend: (event, _hint) => {
-				// Testing for precise falseness. If errortracking is undefined or if getconfig doesn't return anything, errors are not sent.
-				if (
-					getConfig()?.Online?.ErrorTracking !== true ||
-					!this.SentryInitialized
-				)
-					return null;
-				return event;
-			},
-		});
-		this.SentryInitialized = true;
-	}
-
 	setScope(tag: string, data: string) {
 		// Testing for precise falseness. If errortracking is undefined or if getconfig doesn't return anything, errors are not sent.
 		if (getConfig()?.Online?.ErrorTracking !== true || !this.SentryInitialized)
