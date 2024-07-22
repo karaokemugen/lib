b6fd8dbb1bcc7fa6299b3676dcbeb48e1b66460b
Guillaume Lebigot <axel@teri-chan.net>
2024-07-22 16:45:10 +0200

    Move assign issue code from app to lib

--- types/database/user.d.ts
+++ types/database/user.d.ts
@@ -48,4 +48,5 @@ export interface SocialNetworks {
 	anilist?: string;
 	myanimelist?: string;
 	kitsu?: number;
+	gitlab?: string;
 }
--- utils/gitlab.ts
+++ utils/gitlab.ts
@@ -4,6 +4,46 @@ import logger from './logger.js';
 
 const service = 'Gitlab';
 
+/** Assign someone to an issue */
+export async function assignIssue(issue: number, repoName: string, username: string) {
+	let repo = getRepo(repoName);
+	const url = new URL(repo.Git.URL);
+	const userID = await getUserID(repoName, username);
+	const params = {
+		assignee_id: userID,
+	};
+	await HTTP.put(`${url.protocol}//${url.hostname}/api/v4/projects/${repo.Git.ProjectID}/issues/${+issue}`, params, {
+		headers: {
+			'PRIVATE-TOKEN': repo.Git.Password,
+			'Content-Type': 'application/json',
+		},
+		timeout: 25000,
+	});
+}
+
+/** Get user ID from username */
+async function getUserID(repoName: string, username: string) {
+	try {
+		const repo = getRepo(repoName);
+		if (!repo.MaintainerMode) throw 'Maintainer mode is not enabled for this repository';
+		const url = new URL(repo.Git.URL);
+		const res = await HTTP.get(`${url.protocol}//${url.hostname}/api/v4/users`, {
+			params: {
+				username,
+			},
+			headers: {
+				'PRIVATE-TOKEN': repo.Git.Password,
+				'Content-Type': 'application/json',
+			},
+			timeout: 25000,
+		});
+		return res.data[0].id;
+	} catch (err) {
+		logger.error('Unable to get assign user to an issue', { service, obj: err });
+		throw err;
+	}
+}
+
 /** Close an issue */
 export async function closeIssue(issue: number, repoName: string) {
 	const repo = getRepo(repoName);
