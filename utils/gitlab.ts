0b38da5bf18b7c6b2acce66f36e58394647a33ce
Guillaume Lebigot <axel@teri-chan.net>
2025-10-16 12:28:18 +0200

    Add post note to issue function

--- utils/gitlab.ts
+++ utils/gitlab.ts
@@ -4,7 +4,7 @@ import logger from './logger.js';
 
 const service = 'Gitlab';
 
-/** Assign someone to an issue */
+/** Assign someone to an issue. No username means unassignement */
 export async function assignIssue(issue: number, repoName: string, username?: string) {
 	const repo = getRepo(repoName);
 	const url = new URL(repo.Git.URL);
@@ -45,6 +45,24 @@ async function getUserID(repoName: string, username: string) {
 		logger.error('Unable to get assign user to an issue', { service, obj: err });
 		throw err;
 	}
+}
+
+/** Post a note to an issue */
+export async function postNoteToIssue(issue: number, repoName: string, note: string) {
+const repo = getRepo(repoName);
+	const params = {
+		body: note
+	};
+	const url = new URL(repo.Git.URL);
+	const issueURL = `${url.protocol}//${url.hostname}/api/v4/projects/${repo.Git.ProjectID}/issues/${issue}/notes`;
+	logger.debug(`Posting comment to issue ${issueURL} : ${note}`, { service });
+	await HTTP.post(issueURL, params, {
+		headers: {
+			'PRIVATE-TOKEN': repo.Git.Password,
+			'Content-Type': 'application/json',
+		},
+		timeout: 25000,
+	});
 }
 
 /** Close an issue */
