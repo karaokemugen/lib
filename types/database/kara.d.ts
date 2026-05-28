a1b0b6a2697d2831ac104c6e87ab38e7dba70fd7
Guillaume Lebigot <axel@teri-chan.net>
2026-05-28 10:53:10 +0200

    Moved dbstats to lib

--- types/database/kara.d.ts
+++ types/database/kara.d.ts
@@ -148,3 +148,15 @@ export interface KaraListData {
 	data: any;
 	avatars: any;
 }
+
+export interface DBStats {
+	singers: number,
+	songwriters: number,
+	creators: number,
+	authors: number,
+	karas: number,
+	languages: number,
+	series: number,
+	mediasize: number,
+	duration: number
+}
