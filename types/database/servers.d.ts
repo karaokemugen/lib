db2bee81dd0e971a4e3bdbf04255b6af64b421fa
Guillaume Lebigot <axel@teri-chan.net>
2026-05-28 13:46:37 +0200

    Fun with types

--- types/database/servers.d.ts
+++ types/database/servers.d.ts
@@ -1,6 +1,15 @@
+import { DBStats } from "../../../types/database/database.js";
+import { RepositoryManifest } from "../repo.js";
+
 export interface KMServer {
 	domain: string;
 	sid: string;
 	last_seen: Date;
 	flag_banned?: boolean;
+}
+
+export interface KMServerFull extends KMServer {
+	online: boolean;
+	stats?: DBStats;
+	info?: RepositoryManifest;
 }
